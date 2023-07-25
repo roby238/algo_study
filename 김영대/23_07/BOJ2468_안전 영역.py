9d2768eb3d83919b397d6c397238c7145464e4d6
roby238 <roby5100@naver.com>
2023-07-25 21:15:43 +0900

    BOJ2468_안전 영역.py / 1148ms

--- 김영대/23_07/BOJ2468_안전 영역.py
+++ 김영대/23_07/BOJ2468_안전 영역.py
@@ -6,26 +6,11 @@ def solution():
     N = int(read().rstrip())
     h = []
     t = [0 for _ in range(101)]
-    p = [0 for _ in range(N * N)]
     v = [0 for _ in range(N * N)]
     for i in range(N):
         h += list(map(int, read().rstrip().split()))
         for j in range(N):
             t[h[i * N + j]] += 1
-
-    def getParent(x):
-        if x == p[x]:
-            return x
-        p[x] = getParent(p[x])
-        return p[x]
-
-    def setParent(a, b):
-        pa = getParent(a)
-        pb = getParent(b)
-        if pa > pb :
-            p[pa] = pb
-        else:
-            p[pb] = pa
 
     def isValid(s, n):
         return (0 <= n < N * N) and ((s // N == n // N) or (s % N == n % N))
@@ -42,24 +27,19 @@ def solution():
                 if not isValid(s, n):
                     continue
                 if h[n] > height and v[n] == 0:
-                    setParent(s, n)
                     q.append(n)
                     v[n] = 1
 
-
     def simulation(height):
         global ans
+        cnt = 0
         for i in range(N * N):
             v[i] = 0
-            p[i] = i
         for i in range(N * N):
             if height >= h[i] or v[i] == 1:
                 continue
             bfs(i, height)
-        cnt = 0
-        for i in range(N*N):
-            if p[i] == i and h[p[i]] > height:
-                cnt += 1
+            cnt += 1
         ans = max(cnt, ans)
 
     for i in range(1, 101):
