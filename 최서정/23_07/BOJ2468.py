9670af2dd13a218f40b2e4006359404c57efff3d
최서정 <chol0824@naver.com>
2023-07-27 03:01:36 +0900

    BOJ2468 / 780ms

--- 최서정/23_07/BOJ2468.py
+++ 최서정/23_07/BOJ2468.py
@@ -1,11 +1,11 @@
 from collections import deque
 
 n = int(input())
-area = [[0 for _ in range(n)] for _ in range(n)]
-a_max , safe_max = 0,0
+area,height = [],[]
+a_max , safe_max = 0,1
 dx,dy = (-1,0,1,0) , (0,1,0,-1)
 
-def bfs(x,y):
+def bfs(x,y,h,visited):
   q = deque()
   visited[x][y] = True
   q.append((x,y))
@@ -14,28 +14,28 @@ def bfs(x,y):
     for i in range(4):
       nx = x + dx[i]
       ny = y + dy[i]
-      if 0 <= nx < n and 0 <= ny < n:
-        if area[nx][ny] > am and not visited[nx][ny]:
-          visited[nx][ny] = True
-          q.append((nx,ny))
+      if not (0 <= nx < n and 0 <= ny < n) :
+        continue
+      if visited[nx][ny]:
+        continue
+      if area[nx][ny] > h:
+        visited[nx][ny] = True
+        q.append((nx,ny))
   return 1
 
 for i in range(n):
   a = list(map(int,input().split()))
+  area.append(a)
   for j in range(n):
-    area[i][j] = int(a[j])
-    a_max  = max(area[i][j],a_max)
+    if area[i][j] in height: continue
+    else: height.append(area[i][j])
 
-for am in range(1,a_max+1):
+for h in height:
   cnt = 0
   visited = [[False for _ in range(n)] for _ in range(n)]
-
   for i in range(n):
     for j in range(n):
-      if area[i][j] > am and not visited[i][j]:
-        cnt += bfs(i,j)
-
-  if not cnt: cnt = 1
-  safe_max = max(cnt,safe_max)
-        
+      if area[i][j] > h and not visited[i][j]: cnt += bfs(i,j,h,visited)
+  safe_max = max(cnt,safe_max)     
+  
 print(safe_max)
